0e012b87dcf9bb984380b041f475c861374187b0
cotaie <cotaie@yahoo.com>
2024-12-27 10:55:57 +0200

    allow query parameters to be written in any order

--- handlers.ts
+++ handlers.ts
@@ -1,13 +1,11 @@
-type QueryParamsType = { [key: string]: string }
-
 const textHtmlHeaders = new Headers({"Content-Type": "text/html"})
 const imageHeaders = new Headers({"Content-Type": "image/*"})
 
-const getQueryParams = (request: Request): QueryParamsType => {
+const getQueryParams = (request: Request): Map<string,string> => {
     const url = new URL(request.url)
     const queryParams = url.searchParams
-    const args: QueryParamsType = {}
-    queryParams.forEach((value, key) => args[key] = value)
+    const args = new Map<string, string>()
+    queryParams.forEach((value, key) => args.set(key, value))
     console.log("ARGS: ", args)
     return args
 }
@@ -24,7 +22,17 @@ export const sendResponse = (location: string) => async (req: Request): Promise<
         const uint8Array = new Uint8Array(buffer);
 
         const queryParams = getQueryParams(req)
-        const queryParamsValues = Object.values(queryParams)
+
+        const queryParamsValues = ((queryParams: Map<string,string>) => {
+            const queryParamsValues = [] as Array<string>
+            queryParamsValues[0] = queryParams.get('image_name') ?? ""
+            queryParamsValues[1] = queryParams.get('smoothing_type')  ?? ""
+            queryParamsValues[2] = queryParams.get('kernel_size') ?? ""
+            queryParamsValues[3] = queryParams.get('signal_color') ?? queryParams.get('convolution_mode') ?? queryParams.get('border_mode') ?? ""
+            queryParamsValues[4] = queryParams.get('sigma_space') ?? queryParams.get('median_type') ?? ""
+            return queryParamsValues
+        })(queryParams)
+
         const command = new Deno.Command("python", { args: [location, ...queryParamsValues], stdin: "piped", stdout: "piped", stderr: "piped" })
 
         const process = command.spawn();
@@ -38,7 +46,7 @@ export const sendResponse = (location: string) => async (req: Request): Promise<
         console.log("SRDERR: ", process.stderr)
         const stdout = output.stdout
         const headers = new Headers(imageHeaders)
-        headers.set("Content-Disposition", `inline; filename="${queryParams.image_name}"`);
+        headers.set("Content-Disposition", `inline; filename="${queryParams.get('image_name')}"`);
         headers.set("Content-Type", `image/*`);
         //headers.set("X-Time-Taken", `${timeTaken}`)
         return new Response(stdout, {
